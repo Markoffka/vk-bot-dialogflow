8dedb186262e53f1489a8e7eb376bf5717175dc0
Markoffka <inutron@gmail.com>
2018-12-15 01:52:17 +0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,5 +1,17 @@
 const { VK, Keyboard } = require('vk-io');
-
+const http = require('http')
+var port = PROCESS.ENV.PORT ||8080;
+const requestHandler = (request, response) => {
+    console.log(request.url)
+    response.end('Hello Node.js Server!')
+}
+const server = http.createServer(requestHandler)
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+})
 const vk = new VK();
 
 vk.setOptions({
