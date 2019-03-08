30b5369f7f8c2a34e1ccf84070122ea297729066
Markoffka <markoffka.new@gmail.com>
2019-03-08 15:42:08 +0500

    repair token

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const server = require('./server')
 
 server.start({
   port: process.env.PORT || 80,
-  ip: process.env.IP || '0.0.0.0'
+  ip: process.env.IP || 'localhost'
 })
 
 bot.run({
-  token: process.env.TOKEN || require('./config.json').token,
-  id : process.env.groupID || require('./config.json').groupId,
+  token: process.env.TOKEN,
+  id : process.env.groupID,
   mode: 'polling', // webhook or polling
   bot,
   server
