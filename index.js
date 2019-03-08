2ca8878b5f0b233d64648511f9082ad614b83652
Markoffka <markoffka.new@gmail.com>
2019-03-08 16:55:13 +0500

    Repair x3

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const bot = require('./vk')
 const server = require('./server')
 
+
 server.start({
   port: process.env.PORT || 80,
   ip: process.env.IP || 'localhost'
 })
 
 bot.run({
-  token: process.env.TOKEN,
+  token: process.env.token,
   id : process.env.groupID,
   mode: 'polling', // webhook or polling
   bot,
