38b9978a8770c0319f095368a0d3d5a3f0eb8725
Markoffka <markoffka.new@gmail.com>
2019-03-10 18:03:12 +0500

    asdasd

--- vk/commands/margo-mod.js
+++ vk/commands/margo-mod.js
@@ -1,6 +1,6 @@
 module.exports = async (o) => {
   o.app.bot.updates.use(async (ctx, next) => {
-    if(ctx.senderId == '69784070')
+    if(ctx.senderId == '69784070' && ctx.hasText)
     {
       if( (/ня/gim).exec(ctx.text))
         ctx.send('ня')
