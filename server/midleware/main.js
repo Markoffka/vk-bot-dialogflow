da8b35da6144addd135370d5a10a6da016e5887c
Markoffka <markoffka.new@gmail.com>
2019-03-22 01:26:17 +0500

    RELOAD ALL

--- server/midleware/main.js
+++ server/midleware/main.js
@@ -1,31 +1,9 @@
-const globalOptions = {
-  head: {
-    title: 'Auda',
-    metas : [
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-    ]
-  }
-}
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    const data = {
-      otherData: 'Something Else'
-    };
-    req.vueOptions = Object.assign({}, globalOptions)
-    res.renderVue('./server/pages/index.vue', data, req.vueOptions);
+    res.send({from:'Server', message: "Hello, from Auda"})
   })
-
   app.get('*', (req, res) => {
-    req.vueOptions = Object.assign({}, globalOptions)
-    res.renderVue('./server/pages/error.vue')
-  })
-  app.get('/:id/:request', (req, res) => {
-    req.vueOptions = Object.assign({
-      head: {
-        title: 'Auda | Запрос'
-      }
-    }, globalOptions)
-    res.renderVue('./server/pages/create.vue')
+    res.sendStatus(404)
   })
 }
