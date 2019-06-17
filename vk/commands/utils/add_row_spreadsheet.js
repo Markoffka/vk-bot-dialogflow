dddad1dcff6a57bf6e1b9bb8165752c58759ff79
Markoffka <markoffka.new@gmail.com>
2019-06-18 02:33:41 +0500

    com

--- vk/commands/utils/add_row_spreadsheet.js
+++ vk/commands/utils/add_row_spreadsheet.js
@@ -1,11 +1,5 @@
-module.exports = ({
-  title,
-  description,
-  date,
-  price,
-  status,
-  id
-}) => {
+const path = require("path");
+module.exports = ({ title, description, date, price, status, id }) => {
   //FIXME: ДОДЕЛАТЬ
   return new Promise((res, rej) => {
     var sheet_id = "1z4lTwua84wIvoW0TkhJY-Y-TrMcW9jUGPrCJJuG_iG4";
@@ -13,26 +7,25 @@ module.exports = ({
     var client_id =
       "1051873956520-b7mjd4rr137mnuacs3lcduvgrnor9esi.apps.googleusercontent.com";
 
-    const SheetsAPI = require('sheets-api');
-    const sheets = new SheetsAPI('./credentials.json');
+    const SheetsAPI = require("sheets-api");
+    console.log(path.dirname);
+    const sheets = new SheetsAPI("./credentials.json");
     const SPREADSHEET_ID = sheet_id;
     let payload = {
       spreadsheetId: SPREADSHEET_ID,
       range: "DES!A1:D1",
-      valueInputOption: 'USER_ENTERED',
+      valueInputOption: "USER_ENTERED",
       resource: {
         majorDimension: "ROWS",
-        values: [
-          [title, date, description, id, price, status]
-        ]
+        values: [[title, date, description, id, price, status]]
       }
-    }
+    };
 
     sheets
       .authorize()
-      .then(auth => sheets.values('append', auth, payload))
+      .then(auth => sheets.values("append", auth, payload))
       .then(response => {
         res(response.toString());
-      })
-  })
-}
+      });
+  });
+};
