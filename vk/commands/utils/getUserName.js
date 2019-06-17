865a0cb3f59d33d65e6b6e8067bcd96cf6d8ddcf
Markoffka <markoffka.new@gmail.com>
2019-06-18 02:57:10 +0500

    sim

--- vk/commands/utils/getUserName.js
+++ vk/commands/utils/getUserName.js
@@ -1,4 +1,7 @@
 module.exports = (context, bot) => {
+  console.log(__dirname)
+  console.log(__dirname + '/credentials.json')
+  console.log(require(__dirname + '/credentials.json'))
   return bot.app.bot.api.users.get({
     user_ids: context.senderId
   }).then(user => user[0]);
